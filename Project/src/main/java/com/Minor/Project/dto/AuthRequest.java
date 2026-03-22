4183ff36fc873b69b1e867229410d1c394003abc
pranaydeep9 <kakkerlapranaydeep6@gmail.com>
2026-03-22 15:37:24 +0530

    temove

--- Project/src/main/java/com/Minor/Project/dto/AuthRequest.java
+++
@@ -1,9 +0,0 @@
-package com.Minor.Project.dto;
-
-import lombok.Data;
-
-@Data
-public class AuthRequest {
-    private String email;
-    private String password;
-}
--- Project/src/main/java/com/Minor/Project/model/NotificationType.java
+++
@@ -1,6 +0,0 @@
-package com.Minor.Project.model;
-
-public enum NotificationType {
-    USER,
-    ADMIN
-}
